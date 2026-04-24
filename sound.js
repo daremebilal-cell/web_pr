3e6418c6f408a81dd068e747a8ab5c2a2d787439
daremebilal <daremebilal@gmail.com>
2026-04-24 16:40:36 +0100

    The first version oh web project

---
+++ sound.js
@@ -0,0 +1,25 @@
+function playSound(num) {
+            const audio = document.getElementById('s' + num);
+            if (!audio) return;
+            audio.play().catch(e => alert('Error: ' + e.message));
+        }
+
+
+
+if (key === 'a' || key === 'A') {
+            playSound(1);
+        } else if (key === 's' || key === 'S') {
+            playSound(2);
+        } else if (key === 'd' || key === 'D') {
+            playSound(3);
+        } else if (key === 'f' || key === 'F') {
+            playSound(4);
+        } else if (key === 'g' || key === 'G') {
+            playSound(5);
+        } else if (key === 'h' || key === 'H') {
+            playSound(6);
+        } else if (key === 'j' || key === 'J') {
+            playSound(7);
+        } else if (key === 'k' || key === 'K') {
+            playSound(8);
+        }
